17db1f1d34b975ac8551fc1800a16d0c76c7cb28
Balaji Shelar <sbalaji3562@gmail.com>
2026-01-11 19:38:48 +0530

    git practise

---
+++ javascript-training/assignments/assignment-git-test.js
@@ -0,0 +1,93 @@
+//*****Assignment 1*****//
+//Temperature of a city in degrees Celsius: 25.5
+
+let cityTemperature = 33.4;
+console.log(cityTemperature);
+
+//Whether a customer has placed an order: true or false
+let isCustomerOrderPlaced = true;
+console.log(isCustomerOrderPlaced)
+
+//Person's phone number: "123-456-7890"
+let phoneNum = 123-456-7890;
+console.log(phoneNum);
+
+//Amount of money in a customer's bank account: 1000.50
+let bankAccountMoney= 1000.50;
+console.log(bankAccountMoney);
+
+//Person's email address: "john.doe@example.com"
+let emailAddress = "john.doe@example.com";
+console.log(emailAddress);
+
+//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+
+let location = ["latitude", "longitude"];
+let locationValue =["latitude", 37.7749, "longitude", -122.4194];
+
+console.log(locationValue);
+
+//Person's marital status: true or false
+
+let isPersonMarried = true;
+console.log(isPersonMarried);
+
+//Person's occupation: "Software Engineer"
+let occupation= "Software Engineer";
+console.log(occupation);
+
+//Person's favourite colour: "Blue"
+let favColour= "Blue";
+console.log(favColour);
+
+//Current year: 2023
+let currentYear= 2023;
+console.log(currentYear);
+
+//Number of followers on a social media platform: 1,000,000
+
+let numOfSocialMedPlatofrm= 1000000;
+console.log(numOfSocialMedPlatofrm);
+
+//Rating of a movie: 7.5
+let floatnum1= 7.5;
+console.log(floatnum1);
+
+
+//Person's blood type: 'A'
+let bloodType= "A";
+console.log(bloodType);
+
+
+//Title of a book: "To Kill a Mockingbird"
+let bookTitle= "To Kill a Mockingbird";
+console.log(bookTitle);
+
+
+//Number of employees in a company: 500
+let empNum= 500;
+console.log(empNum);
+
+//Time of an event: 2:30 PM
+let eventTime= '2:30';
+console.log(eventTime);
+
+
+//Name of a country: "United States"
+let country= "United States";
+console.log(country);
+
+
+//Person's eye color: "Brown"
+let eyeColour= "Brown";
+console.log(eyeColour);
+
+
+//Person's birthplace: "New York City"
+let birthplace= "New York City";
+console.log(birthplace);
+
+
+//Distance between two cities: 200.5
+let distFloat= 200.5;
+console.log(distFloat);
